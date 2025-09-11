1f67835652285976d202d7a00a61dde20f4e10d3
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-09-11 19:33:08 +0530

    seconLargest

---
+++ secondLargetElement.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class secondLargetElement {
+    public static void main(String[] args) {
+        int arr[]={1,2,4,5,6,6};
+        Arrays.sort(arr);
+        int largest=arr[arr.length-1];
+        int secondLarget=-1;
+        for(int i=arr.length-2;i>0;i--){
+            if(arr[i]!=largest){
+               
+                secondLarget=arr[i];
+                break;
+            }
+            
+        }
+        System.out.println(secondLarget);
+        }
+
+}
