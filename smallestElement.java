22e2fc3a78e7f90da34d5aeaa4be3e5bc1b08b2a
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-09-19 19:50:01 +0530

    smallestElement

---
+++ smallestElement.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class smallestElement {
+    public static void main(String[] args) {
+        int arr[]={5,1,2,4,5};
+        Arrays.sort(arr);
+        System.out.println(arr[0]);
+    }
+}
