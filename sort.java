28f2a2fd556bc4b24fe3a74dd360482dc7a76144
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-09-19 20:50:03 +0530

    sort

---
+++ sort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class sort {
+    public static void main(String[] args) {
+        int [] arr = new int [] {10, 40, 30, 20}; 
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
